70eca024f6e8732bc5a7941a1dad5fef5e839502
Untung Budiman <untungbudiman.dev@gmail.com>
2025-03-08 12:03:33 +0700

    fix

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,6 +1,5 @@
 import { useTheme } from 'next-themes'
 import { useEffect, useRef, useState } from 'react'
-import { flushSync } from 'react-dom'
 
 const isBrowser = typeof window !== 'undefined'
 
