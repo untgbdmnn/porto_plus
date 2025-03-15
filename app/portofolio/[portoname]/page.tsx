54fc446f701197a0fca91ca3290b75662ed73965
Untung Budiman <untungbudiman.dev@gmail.com>
2025-03-15 12:38:49 +0700

    fix

--- app/portofolio/[portoname]/page.tsx
+++ app/portofolio/[portoname]/page.tsx
@@ -1,19 +1,26 @@
-// app/portofolio/[portoname]/page.tsx
 import React from 'react';
 
 type ParamsProps = {
-    params: {
-        portoname: string;
-    };
+  params: {
+    portoname: string;
+  };
 };
 
 export default function DetailPorto({ params }: ParamsProps) {
-    const { portoname } = params;
+  const { portoname } = params;
 
-    return (
-        <div>
-            <h1>Detail Portofolio</h1>
-            <p>Nama Portofolio: {portoname}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Detail Portofolio</h1>
+      <p>Nama Portofolio: {portoname}</p>
+    </div>
+  );
+}
+
+// Generate static paths
+export async function generateStaticParams() {
+  return [
+    { portoname: 'project-1' },
+    { portoname: 'project-2' },
+  ];
 }
